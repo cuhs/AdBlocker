c93531fbbf1d57d44f4f16d7430671ab2c16d7e4
cuhs <alexhu00374@gmail.com>
2024-10-30 19:31:11 -0700

    Started implementing video ad blocks

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,24 +1,12 @@
-(function() {
-    'use strict';
+const adElements = document.querySelectorAll('[id*="ad-"], [id*="-ad"], [class*="ad-"], [class*="-ad"]');
 
-    // Function to remove video ads
-    function removeAds() {
-        const adSelectors = [
-            'video-ad', // Example selector for video ads
-            '.ad-container', // Example selector for ad containers
-            '.ytp-ad-module' // Example selector for YouTube ads
-        ];
-
-        adSelectors.forEach(selector => {
-            const ads = document.querySelectorAll(selector);
-            ads.forEach(ad => ad.remove());
-        });
+adElements.forEach(element => {
+    if (element instanceof HTMLVideoElement) {
+        element.currentTime = element.duration || 0;
+    } else {
+        const videos = element.getElementsByTagName('video');
+        for (let video of videos) {
+            video.currentTime = video.duration || 0;
+        }
     }
-
-    // Observe changes in the DOM to catch dynamically loaded ads
-    const observer = new MutationObserver(removeAds);
-    observer.observe(document.body, { childList: true, subtree: true });
-
-    // Initial call to remove ads
-    removeAds();
-})();
+});
