4915fb2eb55fb62186e87753c1514ef3ce8d2127
cuhs <alexhu00374@gmail.com>
2024-11-04 11:28:55 -0800

    CNN video ads blocked

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,14 +1,24 @@
-const adElements = document.querySelectorAll('[id*="ad-"], [id*="-ad"], [class*="ad-"], [class*="-ad"]');
-
-adElements.forEach(element => {
-    if (element instanceof HTMLVideoElement) {
-        while(element.currentTime < element.duration)
-            element.currentTime+=10;
-    } else {
-        const videos = element.getElementsByTagName('video');
-        for (let video of videos) {
-            while(video.currentTime < video.duration)
-                video.currentTime+=10;
+const skipAds = () => {
+    const adElements = document.querySelectorAll('[id*="ad-"], [id*="-ad"], [class*="ad-"], [class*="-ad"], video');
+    
+    adElements.forEach(element => {
+        if (element instanceof HTMLVideoElement) {
+            if (isFinite(element.duration)) {
+                element.currentTime = element.duration; 
+            }
+        } else {
+            const videos = element.getElementsByTagName('video');
+            for (let video of videos) {
+                if (isFinite(element.duration)) {
+                    video.currentTime = video.duration;
+                }
+            }
         }
-    }
-});
+    });
+};
+
+
+const observer = new MutationObserver(skipAds);
+observer.observe(document.body, { childList: true, subtree: true });
+
+skipAds();
