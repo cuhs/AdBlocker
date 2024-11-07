a28dc47e100feaf44ecc8276bb654409183d904c
cuhs <alexhu00374@gmail.com>
2024-11-06 19:03:05 -0800

    CNN ads blocked

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,20 +1,22 @@
+
 const skipAds = () => {
-    const adElements = document.querySelectorAll('[id*="ad-"], [id*="-ad"], [class*="ad-"], [class*="-ad"], video');
-    
+    const adElements = document.querySelectorAll('[id*="ad-"] video, [id*="-ad"] video, [class*="ad-"] video, [class*="-ad"] video');
+        
     adElements.forEach(element => {
         if (element instanceof HTMLVideoElement) {
             if (isFinite(element.duration)) {
-                element.currentTime = element.duration; 
+                element.currentTime += 3; 
             }
         } else {
             const videos = element.getElementsByTagName('video');
             for (let video of videos) {
-                if (isFinite(element.duration)) {
-                    video.currentTime = video.duration;
+                if (isFinite(element.duration)) {                        
+                    video.currentTime += 3;
                 }
             }
         }
     });
+
 };
 
 
